1bc0d26a10c3452add8bb44565b89f932e7bea33
Max <bludux595@gmail.com>
2021-05-31 21:22:05 -0400

    yes

--- src/main/java/blueduck/tidbits/Tidbits.java
+++ src/main/java/blueduck/tidbits/Tidbits.java
@@ -19,8 +19,8 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("tidbits")
+public class Tidbits
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
