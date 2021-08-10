4db8e8259243c70144cff9b32053aae40dec12bd
Max <bludux595@gmail.com>
2021-08-10 11:31:08 -0400

    Added new village houses for DJ and lumberjack

--- src/main/java/blueduck/tidbits/registry/TidbitsVillagers.java
+++ src/main/java/blueduck/tidbits/registry/TidbitsVillagers.java
@@ -1,17 +1,31 @@
 package blueduck.tidbits.registry;
 
 import blueduck.tidbits.Tidbits;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.mojang.datafixers.util.Either;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.merchant.villager.VillagerProfession;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.village.PointOfInterestType;
+import net.minecraft.world.gen.feature.jigsaw.JigsawPattern;
+import net.minecraft.world.gen.feature.jigsaw.JigsawPiece;
+import net.minecraft.world.gen.feature.jigsaw.LegacySingleJigsawPiece;
+import net.minecraft.world.gen.feature.structure.*;
+import net.minecraft.world.gen.feature.template.ProcessorLists;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.List;
+import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class TidbitsVillagers {
 
@@ -61,11 +75,56 @@ public class TidbitsVillagers {
     public static void init() {
         PROFESSIONS.register(FMLJavaModLoadingContext.get().getModEventBus());
         POI_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        addHouses();
     }
 
     public static RegistryObject<VillagerProfession> condRegister (String registryName, Supplier<VillagerProfession> profession, Supplier<Boolean> condition) {
         if (condition.get())
             return PROFESSIONS.register(registryName, profession);
         return null;
+    }
+
+    public static void addHouses() {
+        PlainsVillagePools.bootstrap();
+        TaigaVillagePools.bootstrap();
+        SnowyVillagePools.bootstrap();
+        DesertVillagePools.bootstrap();
+        SavannaVillagePools.bootstrap();
+
+        if (Tidbits.CONFIG.ENGINEER.get()) {
+
+        }
+
+        if (Tidbits.CONFIG.LUMBERJACK.get()) {
+            addToPool(new ResourceLocation("village/plains/houses"), new ResourceLocation("tidbits:village/lumberjack_plains"), 20);
+            addToPool(new ResourceLocation("village/snowy/houses"), new ResourceLocation("tidbits:village/lumberjack_snowy"), 20);
+            addToPool(new ResourceLocation("village/taiga/houses"), new ResourceLocation("tidbits:village/lumberjack_taiga"), 20);
+            addToPool(new ResourceLocation("village/desert/houses"), new ResourceLocation("tidbits:village/lumberjack_desert"), 20);
+            addToPool(new ResourceLocation("village/savanna/houses"), new ResourceLocation("tidbits:village/lumberjack_savanna"), 20);
+        }
+
+        if (Tidbits.CONFIG.CONTRACTOR.get()) {
+
+        }
+
+        if (Tidbits.CONFIG.DISC_JOCKEY.get() && Tidbits.CONFIG.DISCO_TILES.get()) {
+            addToPool(new ResourceLocation("village/plains/houses"), new ResourceLocation("tidbits:village/dj_stage_plains"), 8);
+            addToPool(new ResourceLocation("village/snowy/houses"), new ResourceLocation("tidbits:village/dj_stage_snowy"), 8);
+            addToPool(new ResourceLocation("village/taiga/houses"), new ResourceLocation("tidbits:village/dj_stage_taiga"), 8);
+            addToPool(new ResourceLocation("village/desert/houses"), new ResourceLocation("tidbits:village/dj_stage_desert"), 8);
+            addToPool(new ResourceLocation("village/savanna/houses"), new ResourceLocation("tidbits:village/dj_stage_savanna"), 8);
+        }
+
+    }
+
+
+
+    private static void addToPool(ResourceLocation pool, ResourceLocation toAdd, int weight) {
+        JigsawPattern old = WorldGenRegistries.TEMPLATE_POOL.get(pool);
+        List<JigsawPiece> shuffled = old != null ? old.getShuffledTemplates(new Random()) : ImmutableList.of();
+        List<Pair<JigsawPiece, Integer>> newPieces = shuffled.stream().map(p -> new Pair<>(p, 1)).collect(Collectors.toList());
+        newPieces.add(new Pair<>(new LegacySingleJigsawPiece(Either.left(toAdd), () -> ProcessorLists.EMPTY, JigsawPattern.PlacementBehaviour.RIGID), weight));
+        ResourceLocation name = old.getName();
+        Registry.register(WorldGenRegistries.TEMPLATE_POOL, pool, new JigsawPattern(pool, name, newPieces));
     }
 }
